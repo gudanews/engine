87ecede05bd086fa85d443c94a576d7db2e111f0
gudaman <houjiahua@hotmail.com>
2020-07-27 20:37:05 -0700

    Add cronjob script

---
+++ crawler.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export PYTHONPATH="./:/Library/Python/3.7/site-packages"
+export PATH="$PATH:/usr/local/bin/:/usr/local/git/bin:/sw/bin/:/usr/local/bin:/usr/local/:/usr/local/sbin:/usr/local/mysql/bin"
+export ENVIRONMENT="PRODUCTION"
+export BROWSER="HEADLESS_CHROME"
+
+ps -aux | grep "[p]ython" | grep "[c]rawler" > /dev/null 2>&1
+if [[ "$?" != "0" ]]; then
+	cd /home/pi/workspace/gudanews
+	/usr/bin/git checkout -- *
+	/usr/bin/git checkout master
+	/usr/bin/git pull
+	/usr/bin/pip3 install -r /home/pi/workspace/gudanews/requirements.txt
+	if [ `ls -lad "/tmp/logs/gudanews.log" | awk '{print $5}'` -gt 1000000 ]; then mv /home/pi/logs/gudanews.log "/home/pi/logs/gudanews_`date '+%Y-%m-%d_%H-%M'`.log"; fi
+	killall chromedriver
+	killall chromium
+	/usr/bin/python3 /home/pi/workspace/gudanews/crawler >> /home/pi/logs/gudanews.log 2>&1
+	killall chromedriver
+	killall chromium
+fi
