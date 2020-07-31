649c9da29d7de1c984f47750e84f17e2c5053fdb
gudaman <houjiahua@hotmail.com>
2020-07-30 23:16:31 -0700

    Update sql backup

--- gudanews_schema.sql
+++ gudanews_schema.sql
@@ -16,40 +16,21 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `data`
+-- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `data`;
+DROP TABLE IF EXISTS `comment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `data` (
-  `id` int(32) NOT NULL,
-  `is_indexed` tinyint(1) NOT NULL DEFAULT 0,
-  `headline_id` int(32) NOT NULL,
-  `image_id` int(32) NOT NULL DEFAULT 0,
-  `source_id` int(16) NOT NULL,
-  `heading` varchar(256) NOT NULL,
-  `datetime` datetime DEFAULT NULL,
-  `url` varchar(512) DEFAULT NULL,
-  `content` text DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used for website news content page';
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `data_image`
---
-
-DROP TABLE IF EXISTS `data_image`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `data_image` (
+CREATE TABLE `comment` (
   `id` int(32) NOT NULL AUTO_INCREMENT,
-  `news_id` int(32) NOT NULL DEFAULT 0,
-  `image_id` int(32) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=FIXED COMMENT='Used for news data and image relation';
+  `parent_id` int(32) NOT NULL DEFAULT 0,
+  `user_id` int(24) NOT NULL DEFAULT 0,
+  `datetime` datetime DEFAULT current_timestamp(),
+  `body` varchar(512) DEFAULT NULL,
+  `likes` int(24) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,21 +47,21 @@ CREATE TABLE `headline` (
   `duplicate_id` int(32) NOT NULL DEFAULT 0,
   `source_id` int(16) NOT NULL DEFAULT 0,
   `image_id` int(32) NOT NULL DEFAULT 0,
-  `data_id` int(32) NOT NULL DEFAULT 0,
+  `news_id` int(32) NOT NULL DEFAULT 0,
   `heading` varchar(256) DEFAULT NULL,
   `url` varchar(512) DEFAULT 'NULL',
-  `datetime` datetime DEFAULT NULL,
+  `datetime` datetime DEFAULT current_timestamp(),
   `snippet` varchar(320) DEFAULT NULL,
-  `quality` int(8) NOT NULL DEFAULT 0,
+  `quality` int(16) NOT NULL DEFAULT 0,
   `view` int(24) NOT NULL DEFAULT 0,
-  `click` int(24) NOT NULL DEFAULT 0,
+  `likes` int(24) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   KEY `source_id` (`source_id`),
-  KEY `image_id` (`image_id`),
-  KEY `id` (`id`) USING BTREE,
-  KEY `duplicate_id` (`duplicate_id`),
-  KEY `data_id` (`data_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2514 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used for website home page';
+  KEY `is_processed` (`is_processed`),
+  KEY `is_displayable` (`is_displayable`),
+  FULLTEXT KEY `snippet` (`snippet`),
+  FULLTEXT KEY `heading` (`heading`)
+) ENGINE=MyISAM AUTO_INCREMENT=2684 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used for website home page';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -96,7 +77,94 @@ CREATE TABLE `image` (
   `thumbnail` varchar(128) DEFAULT NULL,
   `url` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=1920 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used to store image locally';
+) ENGINE=MyISAM AUTO_INCREMENT=2041 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used to store image locally';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `type_id` int(8) NOT NULL DEFAULT 0,
+  `url` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `is_indexed` tinyint(1) NOT NULL DEFAULT 0,
+  `headline_id` int(32) NOT NULL DEFAULT 0,
+  `image_id` int(32) NOT NULL DEFAULT 0,
+  `source_id` int(16) NOT NULL DEFAULT 0,
+  `datetime` datetime DEFAULT current_timestamp(),
+  `heading` varchar(256) DEFAULT NULL,
+  `url` varchar(512) DEFAULT NULL,
+  `body` text DEFAULT NULL,
+  `likes` int(24) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `headline_id` (`headline_id`),
+  KEY `is_indexed` (`is_indexed`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used for website news content page';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `news_comment`
+--
+
+DROP TABLE IF EXISTS `news_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news_comment` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `news_id` int(32) NOT NULL DEFAULT 0,
+  `comment_id` int(32) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `news_id` (`news_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `news_image`
+--
+
+DROP TABLE IF EXISTS `news_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news_image` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `news_id` int(32) NOT NULL DEFAULT 0,
+  `image_id` int(32) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `news_id` (`news_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=FIXED COMMENT='Used for news data and image relation';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `news_media`
+--
+
+DROP TABLE IF EXISTS `news_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news_media` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `news_id` int(32) NOT NULL DEFAULT 0,
+  `media_id` int(32) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `data_id` (`news_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPACT;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -108,12 +176,14 @@ DROP TABLE IF EXISTS `source`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `source` (
   `id` int(16) NOT NULL,
-  `is_crawling` tinyint(1) NOT NULL DEFAULT 0,
-  `name` varchar(32) NOT NULL,
-  `display_name` varchar(32) NOT NULL,
+  `is_crawling` tinyint(1) NOT NULL,
+  `crawling_url` varchar(512) DEFAULT 'NULL',
+  `name` varchar(32) DEFAULT NULL,
+  `display_name` varchar(32) DEFAULT NULL,
+  `image_id` int(32) NOT NULL DEFAULT 0,
+  `host` varchar(64) DEFAULT NULL,
   `bias` int(8) NOT NULL,
-  `image` varchar(256) DEFAULT NULL,
-  `website` varchar(512) DEFAULT NULL,
+  `quality` int(16) NOT NULL DEFAULT 0,
   `popularity` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='Used to store news source';
@@ -128,4 +198,4 @@ CREATE TABLE `source` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-30 16:16:23
+-- Dump completed on 2020-07-30 22:51:21
