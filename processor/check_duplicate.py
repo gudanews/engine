f92af481b2a861918877c5f302f471e16175496b
9999cn <houjiahua@hotmail.com>
2020-07-31 13:57:26 -0700

    Upload changes

--- processor/check_duplicate.py
+++ processor/check_duplicate.py
@@ -1,4 +1,17 @@
-    from database.news_headline import NewsHeadlineDB
-    from util.common import LoggedTestCase
+from database.headline import HeadlineDB
+from util.common import LoggedTestCase
+from util.clean_list import clean_list
+from database.headline import
+class checkDuplicate:
+    def get_headings(self):
+        db = HeadlineDB()
+        column = ["heading"]
+        existing = db.fetch_db_records(column=column)
+        existing = clean_list(existing)
+        return existing
 
-    class checkDuplicate():
+cd = checkDuplicate()
+print(cd.get_headings())
+for i in range(5):
+    #db.insert_db_record(dict(heading='hello'))
+    pass
